3829592983a47eee65ce81b7c8574c679d173c58
sachin yadav <sachinyadav63928@gmail.com>
2026-03-01 13:26:12 +0530

    add new paragraph

---
+++ Mini-Project/Matching-Card/script.js
@@ -0,0 +1,4 @@
+const button = document.querySelectorAll('.btns')
+const playButton = document.getElementById('#play-btn')
+const resetButton = document.getElementById('#rst-btn')
+const times = document.getElementById('#tme')
