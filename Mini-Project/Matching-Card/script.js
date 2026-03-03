3ed4b58f490294dad7cacc5bebb9fa59584dc499
sachin yadav <sachinyadav63928@gmail.com>
2026-03-03 15:30:28 +0530

    add new paragraph

--- Mini-Project/Matching-Card/script.js
+++ Mini-Project/Matching-Card/script.js
@@ -1,4 +1,17 @@
-const button = document.querySelectorAll('.btns')
-const playButton = document.getElementById('#play-btn')
-const resetButton = document.getElementById('#rst-btn')
-const times = document.getElementById('#tme')
+const button = document.querySelectorAll('.btns');
+const times = document.getElementById('#set-time');
+const playButton = document.getElementById('#play-btn');
+const resetButton = document.getElementById('#rst-btn');
+
+const icons = [
+    "fa-node",
+    "fa-java",
+    "fa-react",
+    "fa-linux",
+    "fa-figma",
+    "fa-github",
+    "fa-angular",
+    "fa-database",
+];
+
+let gameIcons = [...icons , ...icons]
