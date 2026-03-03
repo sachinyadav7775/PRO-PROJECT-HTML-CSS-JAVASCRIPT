04e93a88a3bba7d6a591090557086edaa8072a44
sachin yadav <sachinyadav63928@gmail.com>
2026-03-03 16:37:49 +0530

    add new paragraph

--- Mini-Project/Matching-Card/script.js
+++ Mini-Project/Matching-Card/script.js
@@ -1,7 +1,7 @@
 const button = document.querySelectorAll('.btns');
-const times = document.getElementById('#set-time');
-const playButton = document.getElementById('#play-btn');
-const resetButton = document.getElementById('#rst-btn');
+const times = document.getElementById('set-time');
+const playButton = document.getElementById('play-btn');
+const resetButton = document.getElementById('rst-btn');
 
 const icons = [
     "fa-node",
@@ -10,8 +10,25 @@ const icons = [
     "fa-linux",
     "fa-figma",
     "fa-github",
+    "fa-python",
     "fa-angular",
-    "fa-database",
 ];
 
 let gameIcons = [...icons , ...icons]
+.sort(() => 0.5 * Math.random());
+
+// Assign each button a random icon from gameIcons
+button.forEach((btns, index) => {
+    btns.dataset.icon = gameIcons[index]; // store icon in data attribute
+});
+
+// Add click event to show icon
+button.forEach((btns) => {
+    btns.addEventListener("click", () => {
+        // Flip the card visually
+        btns.classList.add('flipped');
+
+        // Show the icon using Font Awesome
+        btns.innerHTML = `<i class="fa-brands ${btns.dataset.icon}"></i>`;
+    });
+});
