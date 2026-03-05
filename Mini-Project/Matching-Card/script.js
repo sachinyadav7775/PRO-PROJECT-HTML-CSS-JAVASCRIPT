489b42d6454e6c90b8d472b9689f03c809972267
sachin yadav <sachinyadav63928@gmail.com>
2026-03-05 20:30:31 +0530

    add new paragraph

--- Mini-Project/Matching-Card/script.js
+++ Mini-Project/Matching-Card/script.js
@@ -3,6 +3,7 @@ const times = document.getElementById('set-time');
 const playButton = document.getElementById('play-btn');
 const resetButton = document.getElementById('rst-btn');
 
+// Icon Array
 const icons = [
     "fa-node",
     "fa-java",
@@ -14,8 +15,7 @@ const icons = [
     "fa-angular",
 ];
 
-let gameIcons = [...icons , ...icons]
-.sort(() => 0.5 * Math.random());
+let gameIcons = [...icons , ...icons].sort(() => 0.5 * Math.random());
 
 // Assign each button a random icon from gameIcons
 button.forEach((btns, index) => {
@@ -32,3 +32,16 @@ button.forEach((btns) => {
         btns.innerHTML = `<i class="fa-brands ${btns.dataset.icon}"></i>`;
     });
 });
+
+
+// Reset Function
+const Reset = () => {
+    gameIcons = [...icons, ...icons].sort(() => Math.random() - 0.5);
+
+    button.forEach((btn, index) => {
+        btn.classList.remove('flipped');
+        btn.dataset.icon = gameIcons[index];
+        btn.innerHTML = '';
+    });
+};
+resetButton.addEventListener("click", Reset);
